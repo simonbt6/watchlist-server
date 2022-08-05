25920376b184e7666fd766904ad790c7bcc28019
Simon Brisebois-Therrien <simonbt6@gmail.com>
2022-08-05 12:58:53 -0400

    Fixed database disconnect

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-Connection = mysql.createConnection({
+let Connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -8,13 +8,25 @@ Connection = mysql.createConnection({
     port: process.env.DB_PORT
 });
 
-Connection.connect((err) => {
-    if (err) {
-        console.error("Failed to connect to database: %s.", err);
-        process.exit(1);
+let connect = () => {
+    Connection.connect((err) => {
+        if (err) {
+            console.error("Failed to connect to database: %s.", err);
+            process.exit(1);
+        }
+        console.log("Connected to database.");
+    });
+}
+
+
+Connection.on('error', (err) => {
+    console.error('DB ERROR', err);
+    if (err.code == 'PROTOCOL_CONNECTION_LOST') {
+        connect();
     }
-    console.log("Connected to database.");
+    else throw err;
 });
 
+connect();
 
 module.exports = Connection;
